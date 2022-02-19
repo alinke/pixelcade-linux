67a9d7a147e5f3fd1f4d85abca999253ec9d1ac0
Alfred Linke <al@ledpixelart.com>
2022-02-18 23:02:47 -0800

    silent mode

--- batocera/scripts/achievements/pixelcade.sh
+++ batocera/scripts/achievements/pixelcade.sh
@@ -25,10 +25,10 @@ URLENCODED_TITLE=$(rawurlencode "$2")
 URLENCODED_DESC=$(rawurlencode "$3")
 ACHIEVEMENTID=$1
 
-PIXELCADECURRENTMARQUEE=$(curl "http://127.0.0.1:8080/currentgame") #api call that gets the last game selected, returns ex. mame%digdug
+PIXELCADECURRENTMARQUEE=$(curl -s "http://127.0.0.1:8080/currentgame") #api call that gets the last game selected, returns ex. mame%digdug
 CURRENTCONSOLE=$(echo $PIXELCADECURRENTMARQUEE | cut -d "%" -f 1) #ex. mame
 CURRENTGAME=$(echo $PIXELCADECURRENTMARQUEE | cut -d "%" -f 2)  #ex. digdug
 CURRENTGAME=$(rawurlencode "$CURRENTGAME")
 #echo $PIXELCADECURRENTMARQUEE
 PIXELCADEURL="achievements/stream/$CURRENTCONSOLE/$ACHIEVEMENTID?t=$URLENCODED_TITLE%20:%20$URLENCODED_DESC&currentgame=$CURRENTGAME"
-curl -s "$PIXELCADEBASEURL$PIXELCADEURL"
+curl -s "$PIXELCADEBASEURL$PIXELCADEURL" >> /dev/null 2>/dev/null &
