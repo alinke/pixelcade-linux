1e69700c3a10b1f50c946f62f0406427ce401dc3
n-i-x <440158+n-i-x@users.noreply.github.com>
2022-06-21 18:11:30 -0400

    Fix autostart.sh to work with batocera

--- system/autostart.sh
+++ system/autostart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # startup script for a dedicated Pi for Pixelcade, systemd calls this via /etc/systemd/system/pixelcade.service
+PIXELHOME=$HOME/pixelcade
+PATH=$PIXELHOME/jdk/bin:$PATH
 
-cd /home/pi/pixelcade && java -jar pixelweb.jar -b &
+cd $PIXELHOME && java -jar pixelweb.jar -b &
 sleep 10 && curl localhost:8080/arcade/stream/mame/pixelcade
 #sleep 10 && cd /home/pi/pixelcade/system && ./pixelcade-startup.sh
