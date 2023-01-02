8f1282fbde9034229c971ad74995eccfff4c8bac
Alfred Linke <al@ledpixelart.com>
2023-01-01 19:00:34 -0800

    bug fixes

--- batocera/scripts/game-start/01-pixelcade.sh
+++ batocera/scripts/game-start/01-pixelcade.sh
@@ -95,8 +95,8 @@ havehighscore() {
 # Main Code Start Here
 	if [ "$SYSTEM" != "" ] && [ "$GAMENAME" != "" ]; then
   	  #clear the Pixelcade Queue, see http://pixelcade.org/api for info on the Queue feature
-  		#PIXELCADEURL="console/stream/black"
-  		#curl -s "$PIXELCADEBASEURL$PIXELCADEURL" >> /dev/null 2>/dev/null & #this was causing an issue on new pixelweb
+  		PIXELCADEURL="console/stream/black"
+  		curl -s "$PIXELCADEBASEURL$PIXELCADEURL" >> /dev/null 2>/dev/null & #this was causing an issue on new pixelweb
 
 			URLENCODED_GAMENAME=$(rawurlencode "$GAMENAME")
       URLENCODED_TITLE=$(rawurlencode "$GAMETITLE")
