d80ef369bf33e5b02dadb6374fd3b001f41ff32a
Al Linke <al@pixelcade.org>
2024-05-05 02:12:24 -0700

    added logging as without it, pixelcade sometimes does not switch and crashes

--- batocera/scripts/game-end/pixelcade.sh
+++ batocera/scripts/game-end/pixelcade.sh
@@ -5,9 +5,15 @@
 # So when we have exited any game, we'll check if pixelweb is running and then launch it if not
 
 process_name="pixelweb"
-if ! pgrep -x "$process_name" > /dev/null; then
-    cd /userdata/system/pixelcade && ./pixelweb -image "system/batocera.png" &
-fi
 
+# Check if the process is not running
+if ! pgrep -x "$process_name" > /dev/null; then
+    server="localhost"
+    port=8080
+    if ! nc -z "$server" "$port"; then
+        curl -s localhost:8080/quit #to be on the safe side as we get into trouble if we start and it's already running
+        cd /userdata/system/pixelcade && ./pixelweb -image "system/batocera.png" -startup -logfile pixelweb.log & #for some reason we don't get issues when logging is on
+    fi
+fi
 
 
