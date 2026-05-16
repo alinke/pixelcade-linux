fcc5790e28e49f96ae1b0d6fe6e52473c899e358
Al Linke <al@pixelcade.org>
2026-05-16 16:55:49 -0700

    fail safe if screen saver end doesn't call /attract/stop

--- recalbox/scripts/gamelaunch[rungame].sh
+++ recalbox/scripts/gamelaunch[rungame].sh
@@ -97,6 +97,7 @@ havehighscore() {
   		PIXELCADEURL="console/stream/black"
   		curl -s "$PIXELCADEBASEURL$PIXELCADEURL" >> /dev/null 2>/dev/null & #this was causing an issue on new pixelweb
 			sleep 1 #TO DO for some reason, doesn't always work without this, in theory it should not be needed
+			curl -s "${PIXELCADEBASEURL}attract/stop" >> /dev/null 2>/dev/null
 			URLENCODED_GAMENAME=$(rawurlencode "$GAMENAME")
       URLENCODED_TITLE=$(rawurlencode "$GAMETITLE")
       #let's make a call here if this game has high scores
