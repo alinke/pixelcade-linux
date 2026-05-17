20a80deb32a22db63d82bc620bc63370cd0b238d
Al Linke <al@pixelcade.org>
2026-05-16 17:00:49 -0700

    adding attract stop fail safe on game launch

--- emuelec/scripts/game-start/01-pixelcade.sh
+++ emuelec/scripts/game-start/01-pixelcade.sh
@@ -98,6 +98,7 @@ havehighscore() {
   		PIXELCADEURL="console/stream/black"
   		curl -s "$PIXELCADEBASEURL$PIXELCADEURL" >> /dev/null 2>/dev/null &
 			sleep 1 #TO DO for some reason, doesn't always work without this, in theory it should not be needed
+			curl -s "${PIXELCADEBASEURL}attract/stop" >> /dev/null 2>/dev/null
       URLENCODED_GAMENAME=$(rawurlencode "$GAMENAME")
       URLENCODED_TITLE=$(rawurlencode "$GAMETITLE")
       #let's make a call here if this game has high scores
